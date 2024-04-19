f0ffe8175009fe01ed1ae251b5120c2e4667bdf7
julien henry <julienhenry967@gmail.com>
2024-04-19 15:56:46 +0200

    chore: setup dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,25 @@ import Dashboard from "./components/Dashboard/Dashboard";
 import "./App.css";
 
 const App = () => {
+  const userId = 12;
   const {
     data: user,
     isLoading,
     error,
-  } = useFetch("http://localhost:3000/user/12");
+  } = useFetch(`http://localhost:3000/user/${userId}`);
+
+  const { data: userActivity } = useFetch(
+    `http://localhost:3000/user/${userId}/activity`
+  );
+
+  const { data: userAverageSessions } = useFetch(
+    `http://localhost:3000/user/${userId}/average-sessions`
+  );
+
+  const { data: userPerformances } = useFetch(
+    `http://localhost:3000/user/${userId}/performance`
+  );
+
   return (
     <div className="app">
       <Header />
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,8 +1,14 @@
 import DashboardHeader from "./DashboardHeader";
 import "./Dashboard.css";
+import BarChart from "../BarChart/BarChart";
 
 const Dashboard = ({ user }) => {
-  return <DashboardHeader firstName={user.userInfos.firstName} />;
+  return (
+    <>
+      <DashboardHeader firstName={user.userInfos.firstName} />
+      <BarChart />
+    </>
+  );
 };
 
 export default Dashboard;
