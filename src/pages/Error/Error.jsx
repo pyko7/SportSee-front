f183e86a8fb874d0fbda8432fb2c0035f6a58681
julien henry <julienhenry967@gmail.com>
2024-05-03 11:08:51 +0200

    chore: add error page

---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,10 @@
+import "./Error.css";
+const Error = () => {
+  return (
+    <div className="error-page">
+      <p>Une erreur est survenue lors de la récupération des données.</p>
+    </div>
+  );
+};
+
+export default Error;
